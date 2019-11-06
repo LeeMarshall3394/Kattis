ab9141297d45225bbdddce912d59c90044cce80d
Lee Marshall <34558251+LeeMarshall3394@users.noreply.github.com>
2019-11-06 16:09:26 +0000

    Completed 2

---
+++ C#/deathknight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Class1
+{
+    public static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        bool lose = false;
+        int wins = 0;
+        string[] inputs = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            inputs[i] = Console.ReadLine();
+        }
+
+        foreach (string x in inputs)
+        {
+            char[] skills = x.ToCharArray();
+            for (int i = 0; i < skills.Length - 1; i++)
+            {
+                if (skills[i] == 67 && skills[i + 1] == 68)
+                {
+                    lose = true;
+                }
+            }
+            if (!lose)
+            {
+                wins += 1;
+            }
+            lose = false;
+        }
+
+        Console.WriteLine(wins);
+        Console.ReadKey();
+    }
+}
---
+++ C#/estimatingtheareaofacircle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Class1
+{
+    public static void Main(string[] args)
+    {
+        List<string> results = new List<string>();
+        List<string> inputs = new List<string>();
+        bool end = false;
+        
+        do
+        {
+            inputs.Add(Console.ReadLine());
+
+            if (inputs[inputs.Count - 1] != "0 0 0")
+            {
+                string[] input = inputs[inputs.Count - 1].Split(' ');
+                double[] values = new double[input.Length];
+                for (int i = 0; i < 3; i++)
+                {
+                    values[i] = double.Parse(input[i]);
+                }
+
+                double peri = values[0] * 2;
+                double sArea = peri * peri;
+                double tArea = Math.PI * (values[0] * values[0]);
+                double eArea = values[2] / values[1] * sArea;
+
+                results.Add(tArea + " " + eArea);
+            }
+            else
+                end = true;
+        } while (end == false);
+
+        foreach (string x in results)
+        {
+            Console.WriteLine(x);
+        }
+
+        Console.ReadKey();
+    }
+}
